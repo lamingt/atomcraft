0b6f4090521102cb15b49ffbfdd3c649c50b25e5
lamingt <133670825+lamingt@users.noreply.github.com>
2025-11-09 21:35:28 +1100

    done most of home page up to tokamak

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@astrojs/netlify/static";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
