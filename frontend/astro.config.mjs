4ceda7dca13716912c0d9d1eebb39eaf7f5e1046
lamingt <133670825+lamingt@users.noreply.github.com>
2025-11-30 18:51:41 +1100

    finished footer, minor fixes around the home page and began about page

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,12 +6,13 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
+  vite: {
     plugins: [tailwindcss()],
     server: {
-      allowedHosts: ["devserver-redesign--atomcraft-temp.netlify.app"],
+      allowedHosts: ["devserver-redesign--atomcraft-temp.netlify.app"], // for preview servers on netlify
     },
   },
   output: "static",
   adapter: netlify(),
+  site: "https://www.atomcraft.com.au",
 });
